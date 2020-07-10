8f460658ea5bd1a3c8b9ccdea97c44d234426ec2
aloneness <logic9711@foxmail.com>
2020-07-10 12:12:55 +0800

    mybatis plus

---
+++ well-starter-mybatis-plus/src/main/java/com/aloneness/well/mybatis/plus/config/MybatisPlusConfig.java
@@ -0,0 +1,11 @@
+package com.aloneness.well.mybatis.plus.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis plus 配置文件
+ *
+ */
+@Configuration
+public class MybatisPlusConfig {
+}
